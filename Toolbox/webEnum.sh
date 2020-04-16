a9eac6e1af956d8480df0b466be0db2b1aa6c513
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2020-04-15 22:19:56 -0400

    Update webEnum.sh

--- Toolbox/webEnum.sh
+++ Toolbox/webEnum.sh
@@ -1,1 +1,59 @@
+#!/bin/bash
 
+if [[ $# != 2 ]]; then
+	echo "Usage web.sh <http://target> <port>"
+	exit 2
+fi
+
+
+/usr/bin/echo "what is the project name?"
+
+read project
+
+echo -e "\n\n[+] preparing project directories and files\n\n"
+
+/usr/bin/mkdir $project
+/usr/bin/mkdir $project/directories
+git -C $project/directories/ clone  https://github.com/maaaaz/webscreenshot.git
+
+echo -e "\n\n[+] gobusting directories\n\n"
+
+
+/usr/bin/gobuster -q dir -u $1:$2 -w /usr/share/wordlists/dirb/common.txt > $project/directories/dirs.txt || echo -e "\nError with gobuster\n" && exit 2
+
+/usr/bin/gobuster -q dir -u $1:$2 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt >> $project/directories/dirs.txt || echo "\nError with gobuster\n" && exit 2
+
+
+
+sort -u $project/directories/dirs.txt > $project/directories/dir2.txt 
+
+cat $project/directories/dir2.txt | grep -v ^"\[" | grep -v ^\= | grep -v ^"by" | grep -v ^"20" > $project/directories/FinalList.txt
+
+cat $project/directories/FinalList.txt | grep "Status: 1" | rev | cut -c14-  | rev > $project/directories/100status.txt
+
+cat $project/directories/FinalList.txt | grep "Status: 2" | rev | cut -c14-  | rev > $project/directories/200status.txt
+
+cat $project/directories/FinalList.txt | grep "Status: 3" | rev | cut -c14-  | rev > $project/directories/300status.txt
+
+cat $project/directories/FinalList.txt | grep "Status: 4" | rev | cut -c14-  | rev > $project/directories/400status.txt
+
+cat $project/directories/FinalList.txt | grep "Status: 5" | rev | cut -c14-  | rev > $project/directories/500status.txt
+
+
+
+rm $project/directories/dirs.txt
+rm $project/directories/dir2.txt
+
+
+echo -e "\n\n[+] grabbing all the screenshots\n\n"
+
+
+python3 $project/directories/webscreenshot/webscreenshot.py -i $project/directories/100status.txt -o $project/directories/screenshots/100
+
+python3 $project/directories/webscreenshot/webscreenshot.py -i $project/directories/200status.txt -o $project/directories/screenshots/200
+
+python3 $project/directories/webscreenshot/webscreenshot.py -i $project/directories/300status.txt -o $project/directories/screenshots/300
+
+python3 $project/directories/webscreenshot/webscreenshot.py -i $project/directories/400status.txt -o $project/directories/screenshots/400
+
+python3 $project/directories/webscreenshot/webscreenshot.py -i $project/directories/500status.txt -o $project/directories/screenshots/500
