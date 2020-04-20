8dba68b585946b1b832b9b5611453a53658d3ea7
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2020-04-19 23:21:12 -0400

    Update netScan.sh

--- Toolbox/netScan.sh
+++ Toolbox/netScan.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-/usr/bin/echo -e "[+] Scanning for TCP ports\n\n\n"
+
+/usr/bin/echo -e "[+] All scan for all TCP ports\n\n\n"
+
 
 /usr/bin/nmap -p - -T4 -A $1 -oN "TCPports.txt"
 
-/usr/bin/echo -e "[+] Scanning for UDP ports\n\n\n"
+/usr/bin/echo -e "[+] Scanning for top 1000 UDP ports\n\n\n"
 
-/usr/bin/nmap -sU -p -sV $1 -oN "UDPports.txt"
+/usr/bin/nmap -sU  -sV $1 -oN "UDPports.txt"
