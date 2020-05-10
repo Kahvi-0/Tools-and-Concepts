058c17f49675da9e5db8133d07dfc519bfd6862c
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2020-05-09 21:54:18 -0400

    Update netScan.sh

--- Toolbox/netScan.sh
+++ Toolbox/netScan.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-# Add ping
+
+if [[ $# -lt 1 ]]; then
+	echo "Usage recond.sh <target> <option>"
+	echo "options:"
+	echo "vuln       Run vulnerability scan using the NSE vuln script"
+	echo "udp        Run a UDP port scan"
+	echo ""
+	exit 2
+fi
+
+if [[ $1 != ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+	echo "First argument must be an IP address"
+	exit 2
+fi
 
 
-/usr/bin/echo -e "\n\n\n[+] All scan for all TCP ports for $1 \n\n\n"
+if [[ $@ == vuln ]]; then
 
+	/usr/bin/echo -e "[+] Running vulnscan, vulners, and vuln NSE scripts for $1 \n\n\n"
+	/usr/bin/nmap -Pn --script nmap-vulners,vulnscan -sV $1
+	/usr/bin/nmap -Pn --script vuln -sV $1
+fi
+
+if [[ $@ == udp ]]; then
+
+	/usr/bin/echo -e "[+] Scanning for top 1000 UDP ports for $1 \n\n\n"
+	/usr/bin/nmap -Pn -sU  -sV $1 -oN "UDPports.txt"
+
+fi
+
+
+/usr/bin/echo -e "[+] All scan for all TCP ports for $1 \n\n\n"
 /usr/bin/nmap -Pn -p - -T4 -A $1 -oN "TCPports.txt"
-
-/usr/bin/echo -e "\n\n\n[+] Scanning for top 1000 UDP ports for $1 \n\n\n"
-
-/usr/bin/nmap -Pn -sU  -sV $1 -oN "UDPports.txt"
